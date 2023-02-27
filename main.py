f9fe45afb12357175a2ec7205649f943c92bf701
Kaushik K <kaushik.k@biofourmis.com>
2023-02-27 10:43:50 +0530

    testing 11

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ class Calculator:
         return num1 - num2
 
     def multiply(self, num1, num2):
-
+        """This function will be returning the addition of the two numbers"""
         return num1 * num2
